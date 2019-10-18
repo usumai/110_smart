76a8d2a74c11a01d2d6b2de46f8c4c4afcd32455
Lucas <usumai@gmail.com>
2019-10-18 13:17:36 +1100

    auto commit

--- 05_action.php
+++ 05_action.php
@@ -29,6 +29,7 @@ if ($act=='sys_pull_master') {
 	//This file updates the local software with the currently published software
 
 	$output  = shell_exec($addr_git.' init 2>&1'); 
+	$output  = shell_exec($addr_git.' remote set-url https://github.com/usumai/110_smart.git'); 
 	$output .= shell_exec($addr_git.' clean  -d  -f .');
 	$output .= shell_exec($addr_git.' reset --hard');  
 	$output .= shell_exec($addr_git.' pull https://github.com/usumai/110_smart.git');
