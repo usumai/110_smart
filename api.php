9b9a1f9fc5f14bca37e10f5f36c560d4c1b28084
Seagle Maier <seaglem@tpg.com.au>
2020-12-22 10:40:57 +1100

    split and format git responses

--- api.php
+++ api.php
@@ -866,7 +866,7 @@ function updateSoftware() {
 	$servername = "";
 	$username   = "root";
 	$password   = "";
-
+	$output=[];
 	$con = new mysqli($servername, $username, $password);
 	
 	// Check connection
@@ -880,12 +880,19 @@ function updateSoftware() {
 	mysqli_multi_query($con,$sql_save); 
 	
 	$addr_git= ' "\Program Files\Git\bin\git"  ';
-	$output[]  = shell_exec($addr_git.' init 2>&1'); 
-	$output[]  = shell_exec($addr_git.' remote set-url https://github.com/usumai/110_smart.git'); 
-	$output[]= shell_exec($addr_git.' clean  -d  -f .');
-	$output[]= shell_exec($addr_git.' reset --hard');  
-	$output[]= shell_exec($addr_git.' pull https://github.com/usumai/110_smart.git');
+	splitLines($output, shell_exec($addr_git.' init 2>&1')); 
+	splitLines($output, shell_exec($addr_git.' remote set-url https://github.com/usumai/110_smart.git')); 
+	splitLines($output, shell_exec($addr_git.' clean  -d  -f .'));
+	splitLines($output, shell_exec($addr_git.' reset --hard'));  
+	splitLines($output, shell_exec($addr_git.' pull https://github.com/usumai/110_smart.git'));
+	splitLines($output, shell_exec($addr_git.' git rev-parse HEAD'));
 	$result = ["info" => $output];
 	return $result;
+}
+
+function splitLines($entries, $outputText){
+	foreach(explode("\n",$outputText) as $line){
+		$entries[]=$line;
+	}
 }
 ?>
