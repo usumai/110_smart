a05f0399c428dc90572b0a4cd119347a5e8f3c43
Seagle Maier <seaglem@tpg.com.au>
2021-06-11 12:53:39 +1000

    Change Reason Code for Sighted Edit to RE20 from ND10

--- 11_ass.php
+++ 11_ass.php
@@ -19,6 +19,7 @@
                                     v-if="!show_delete_options&&assd.genesis_cat=='nonoriginal'"
                                     class="text-center list-group-item list-group-item-action list-group-item-warning" 
                                     type="button" >Change<br>reason code</button>
+                                    
                             <button @click="select_rc('RE20')"
                                     v-if="!assd.res_reason_code&&assd.genesis_cat=='original'"
                                     class="text-center list-group-item list-group-item-action list-group-item-success mt-3" 
